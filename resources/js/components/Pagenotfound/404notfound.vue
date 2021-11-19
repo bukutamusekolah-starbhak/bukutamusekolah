54830deadd027fb1e76dea3245f56a6cbaa041d2
achmadfadliiskandar <af137357@gmail.com>
2021-11-19 17:35:07 +0700

    cukup menambahkan gambar pada 404 not found

--- resources/js/components/Pagenotfound/404notfound.vue
+++ resources/js/components/Pagenotfound/404notfound.vue
@@ -1,3 +1,7 @@
 <template>
+<div class="container text-center">
+    <img src="https://101red.com/prime/wp-content/uploads/2021/03/24-Error-404-Not-Found-Apa-Artinya-serta-Gimana-Metode-Mengatasinya.jpg" style="width:50%;" alt="">
     <h2>Halaman Tidak ada</h2>
+    <router-link to="/" class="btn btn-primary my-3">Back To Home</router-link>
+</div>
 </template>
