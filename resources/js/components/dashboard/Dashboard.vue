99b9b00988f7af2a81d3b9cb357ffc0ca398ad04
achmadfadliiskandar <af137357@gmail.com>
2021-11-23 16:55:46 +0700

    menambahkan iframe jasma(jam masjid)

--- resources/js/components/dashboard/Dashboard.vue
+++ resources/js/components/dashboard/Dashboard.vue
@@ -30,26 +30,21 @@
         </div>
         <div class="row mt-3 mb-3">
             <div class="col-sm-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.0746162947407!2d106.86784151476996!3d-6.3843711953803695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ebaff005f277%3A0x9fcd41028665eea8!2sSMK%20Taruna%20Bhakti%20Depok!5e0!3m2!1sid!2sid!4v1636446534221!5m2!1sid!2sid" class="w-100 h-100" allowfullscreen="" loading="lazy"></iframe>
+            <p style="text-align: center;">
+            <iframe src="https://jam.jadwalsholat.org/digitalclock/" class="w-100" style="height:70px;"></iframe>
+            <iframe src="https://www.jadwalsholat.org/jadwal-sholat/monthly.php?id=67" style="height:300px;width:100%;"></iframe>
+            </p>
             </div>
-            <div class="col-sm-6 bg-dark pb-3 pt-3 text-white">
-            <h3>Calendar : </h3>
-            <date-picker v-model="date" :config="options"></date-picker>
-            <h3>Jam Hari ini : </h3>
-            <vue-clock />
-            <h3>Website ini di buat dengan :</h3>
+            <div class="col-sm-6 bg-dark text-white" style="height:375px;">
+            <h3 class="pt-2 pb-2">Website ini di buat dengan :</h3>
             <div class="row">
-            <div class="col-md-4 text-center">
-                <i class="fab fa-laravel red fa-3x"></i>
-                <strong class="text-danger">Laravel</strong>
-            </div>
-            <div class="col-md-4">
-                <i class="fab fa-vuejs green fa-3x"></i>
-                <strong class="text-success">Vue Js</strong>
-            </div>
-            <div class="col-md-4">
-                <i class="fab fa-bootstrap purple fa-3x"></i>
-                <strong style="color:#9561e2;">Boostrap 5</strong>
+            <div class="col-md-12 text-center pb-2 pt-2">
+            <i class="fab fa-laravel red fa-4x"></i>
+            <p class="text-danger">Laravel</p>
+            <i class="fab fa-vuejs green fa-4x"></i>
+            <p class="text-success">Vue Js</p>
+            <i class="fab fa-bootstrap purple fa-4x"></i>
+            <p style="color:#9561e2;">Boostrap 5</p>
             </div>
             </div>
             </div>
